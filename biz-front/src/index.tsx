f4d4007529c23936175bb40424fdbf40c1dde921
이은석 <modelism93@naver.com>
2024-01-25 17:54:50 +0900

    [FE] config : react-query, recoil 세팅

--- biz-front/src/index.tsx
+++ biz-front/src/index.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
 
 root.render(
+  // <Suspense fallback={<Spinner />}>
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>
+  //</Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
