19e5cfa54ae84c07ed9d279e1c51ee366ca19583
이은석 <modelism93@naver.com>
2024-02-05 22:55:54 +0900

    [FE] fix: 스크롤 안되는 문제 globalstyle에서 overflow:hidden 삭제

--- biz-front/src/styles/GlobalStyle.tsx
+++ biz-front/src/styles/GlobalStyle.tsx
@@ -37,8 +37,6 @@ const GlobalStyle = createGlobalStyle`
 	body {
 		width: 100vw;
 		height: 100vh;
-		overflow: hidden;
-		font-family: 'NanumSquareNeo';
 		font-weight: 700;
 		touch-action: none;
 	}
