8dc277c77b3b1565757c5277b15596016d05dcd6
이은석 <modelism93@naver.com>
2024-02-07 20:11:59 +0900

    [FE] feat: 뒤로가기 버튼 로직 생성

--- biz-front/src/components/molecules/SignupPage/GoBackNavComp.tsx
+++ biz-front/src/components/molecules/SignupPage/GoBackNavComp.tsx
@@ -2,12 +2,19 @@ import Button from '@/components/atoms/Button/Button';
 import { Text } from '@/components/atoms/Text/Text';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
 import { FaArrowLeft } from 'react-icons/fa6';
+import { useNavigate } from 'react-router';
 
 const GoBackNavComp = () => {
+  const navigate = useNavigate();
+
+  const handleGoback = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <Wrapper $width="80vw" $marginBottom="85px">
-        <Button size="small" option="textButton">
+        <Button size="small" option="textButton" onClick={handleGoback}>
           <FaArrowLeft />
           <Text $marginLeft="10px">뒤로가기</Text>
         </Button>
