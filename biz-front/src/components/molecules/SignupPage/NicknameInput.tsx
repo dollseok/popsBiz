e4abc52f4bbab021abd59f4fa01021eaf14ab605
이은석 <modelism93@naver.com>
2024-02-13 12:24:56 +0900

    [FE] Feat: 닉네임 중복 확인 로직

--- biz-front/src/components/molecules/SignupPage/NicknameInput.tsx
+++ biz-front/src/components/molecules/SignupPage/NicknameInput.tsx
@@ -1,17 +1,41 @@
+import { useCheckNickname } from '@/apis/User/Mutations/useCheckNickname';
 import Button from '@/components/atoms/Button/Button';
 import Input from '@/components/atoms/Input/Input';
 import { Text } from '@/components/atoms/Text/Text';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
+import React, { useState } from 'react';
 
 const NicknameInput = () => {
+  const [nickname, setNickname] = useState('');
+
+  const checkNicknameMutation = useCheckNickname();
+
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newNickname = e.target.value;
+    setNickname(newNickname);
+  };
+
+  const handleCheckNicknameClick = () => {
+    console.log(nickname);
+    checkNicknameMutation.mutate(nickname);
+  };
+
   return (
     <>
       <Text size="body2" $marginBottom="15px">
         닉네임
       </Text>
       <Wrapper option="Row">
-        <Input type="text" placeholder="기업 또는 브랜드명" />
-        <Button size="small">중복 확인</Button>
+        <Input
+          type="text"
+          placeholder="기업 또는 브랜드명"
+          onChange={e => {
+            handleNicknameChange(e);
+          }}
+        />
+        <Button size="small" onClick={handleCheckNicknameClick}>
+          중복 확인
+        </Button>
       </Wrapper>
 
       <Text size="body4" $color="grey1">
