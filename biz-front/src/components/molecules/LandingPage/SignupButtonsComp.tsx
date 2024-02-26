cee6adca7f266aaca254795f95f746471dda6a7c
이은석 <modelism93@naver.com>
2024-02-27 00:45:02 +0900

    [FE] feat: 구글 로그인 요청보내는 페이지로 이동하는 버튼 로직

--- biz-front/src/components/molecules/LandingPage/SignupButtonsComp.tsx
+++ biz-front/src/components/molecules/LandingPage/SignupButtonsComp.tsx
@@ -1,13 +1,22 @@
 import Button from '@/components/atoms/Button/Button';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
 import { PATH } from '@/constants/path';
+import axios from 'axios';
 import { useNavigate } from 'react-router';
 
 const SignupButtonsComp = () => {
+  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const REDIRECT_URI = 'http://localhost:3000/sociallogin';
+
   const navigate = useNavigate();
 
   const handleRouter = (url: string): void => {
     navigate(url);
+  };
+
+  const handleGooglelogin = () => {
+    const googleOAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&scope=email&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = googleOAuthUrl;
   };
 
   return (
@@ -29,7 +38,14 @@ const SignupButtonsComp = () => {
         </Wrapper>
       </Wrapper>
 
-      <Button option="border" $marginBottom="10px" $marginTop="38px">
+      <Button
+        option="border"
+        $marginBottom="10px"
+        $marginTop="38px"
+        onClick={() => {
+          handleGooglelogin();
+        }}
+      >
         구글 이메일로 시작하기
       </Button>
       <Button option="border">페이스북으로 시작하기</Button>
