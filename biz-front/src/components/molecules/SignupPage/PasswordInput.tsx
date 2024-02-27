9236f4478dbcb72b37be519ab6129b27b82a4de4
이은석 <modelism93@naver.com>
2024-02-27 18:57:35 +0900

    [FE] feat: 비밀번호 형식 수정, onChange와 value와 trim()을 통한 공백 제거

--- biz-front/src/components/molecules/SignupPage/PasswordInput.tsx
+++ biz-front/src/components/molecules/SignupPage/PasswordInput.tsx
@@ -1,9 +1,9 @@
 import Input from '@/components/atoms/Input/Input';
 import { Text } from '@/components/atoms/Text/Text';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
-import { signupInfoState } from '@/states/User';
+import { passwordErrorState, signupInfoState } from '@/states/User';
 import { useEffect, useState } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useResetRecoilState, useSetRecoilState } from 'recoil';
 
 const PasswordInput = () => {
   const [firstPassword, setFirstPassword] = useState<string>('');
@@ -12,24 +12,41 @@ const PasswordInput = () => {
     useState<boolean>(true);
   const [validateSecondPassword, setValidateSecondPassword] =
     useState<boolean>(true);
-  const [passwordErrorMention, setPasswordErrorMention] = useState<string>('');
+  const [passwordError, setPasswordError] = useRecoilState(passwordErrorState);
+  const resetPasswordError = useResetRecoilState(passwordErrorState);
 
   const setSignupInfo = useSetRecoilState(signupInfoState);
 
   const handleCheckPassword = (p1: string, p2: string) => {
     if (p1 !== p2) {
+      // 비밀번호 유효성 검사 실패
       setValidateSecondPassword(false);
       setSignupInfo(prev => ({ ...prev, password: '' }));
+      setPasswordError(prev => ({ ...prev, state: true })); // 상태만 체크
     } else {
+      // 비밀번호 유효성 검사 통과
       setValidateSecondPassword(true);
       setSignupInfo(prev => ({ ...prev, password: p1 }));
+      resetPasswordError();
     }
   };
 
+  const onChangeFirstPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const trimmedPassword = e.target.value.trim();
+    console.log(trimmedPassword);
+    setFirstPassword(trimmedPassword);
+  };
+
+  const onChangeSecondPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const trimmedPassword = e.target.value.trim();
+    console.log(trimmedPassword);
+    setSecondPassword(trimmedPassword);
+  };
+
   const onBlurFirstPassword = async (e: React.FocusEvent<HTMLInputElement>) => {
-    const newPassword = e.target.value;
+    const newPassword = e.target.value.trim();
     if (newPassword === '') {
-      setPasswordErrorMention('비밀번호를 입력해주세요');
+      setPasswordError({ state: true, message: '비밀번호를 입력해주세요' });
       setValidateFirstPassword(false);
     }
     setFirstPassword(newPassword);
@@ -43,13 +60,24 @@ const PasswordInput = () => {
   useEffect(() => {
     // 비밀번호 조건 확인
     if (firstPassword !== '') {
-      const reg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
-      if (reg.test(firstPassword)) {
+      const reg1 = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/; // 숫자 & 문자 & 특수문자
+      const reg2 = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/; // 문자 & 숫자
+      const reg3 = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-]).{8,20}$/; // 문자 & 특수문자
+      const reg4 = /^(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/; // 숫자 & 특수문자
+      // 넷 중 하나만 만족하면 통과
+      if (
+        reg1.test(firstPassword) ||
+        reg2.test(firstPassword) ||
+        reg3.test(firstPassword) ||
+        reg4.test(firstPassword)
+      ) {
         setValidateFirstPassword(true);
       } else {
-        setPasswordErrorMention(
-          '비밀번호 형식에 맞지 않습니다. 형식을 다시 확인해주세요.'
-        );
+        setPasswordError({
+          state: true,
+          message: '비밀번호 형식에 맞지 않습니다. 형식을 다시 확인해주세요.',
+        });
+
         setValidateFirstPassword(false);
       }
     }
@@ -68,7 +96,9 @@ const PasswordInput = () => {
         </Text>
         <Input
           type="password"
+          value={firstPassword}
           placeholder="비밀번호를 입력해주세요"
+          onChange={onChangeFirstPassword}
           onBlur={onBlurFirstPassword}
           $marginBottom="10px"
         />
@@ -80,7 +110,7 @@ const PasswordInput = () => {
           <></>
         ) : (
           <Text $color="danger" size="body4" $marginLeft="10px">
-            {passwordErrorMention}
+            {passwordError.message}
           </Text>
         )}
       </Wrapper>
@@ -90,8 +120,10 @@ const PasswordInput = () => {
         </Text>
         <Input
           type="password"
+          value={secondPassword}
           placeholder="비밀번호를 다시 입력해주세요"
           onBlur={onBlurSecondPassword}
+          onChange={onChangeSecondPassword}
           $marginBottom="10px"
         />
 
