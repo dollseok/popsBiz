25fdae12df8645ace19f46bfff6839860b574c3a
이은석 <modelism93@naver.com>
2024-02-27 09:42:25 +0900

    [FE] refactor: 이미지 선택시마다 이미지 PresignedUrl에 저장

--- biz-front/src/pages/SignupPage/SignupPage.tsx
+++ biz-front/src/pages/SignupPage/SignupPage.tsx
@@ -7,28 +7,17 @@ import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
 import { GoBackNavComp } from '@/components/molecules/SignupPage/GoBackNavComp';
 import { SignupComp } from '@/components/organisms/SignupPage/SignupComp';
 import { UserdataComp } from '@/components/organisms/SignupPage/UserdataComp';
-import { agreementState, imageFileState, signupInfoState } from '@/states/User';
+import { agreementState, signupInfoState } from '@/states/User';
 import { useRecoilValue } from 'recoil';
 
 const SignupPage = () => {
   const signupInfo = useRecoilValue(signupInfoState); // 회원가입에 요구되는 데이터
   const agreement = useRecoilValue(agreementState); // 필수 동의 데이터
-  const imageFile = useRecoilValue(imageFileState);
 
-  const uploadProfileImage = useUploadProfileImage();
   const signupEmail = useSignupEmail();
-
-  const handleUploadImage = () => {
-    if (imageFile) {
-      const blob = new Blob([imageFile], { type: 'image/jpeg' });
-      console.log('blob Image :', imageFile);
-      uploadProfileImage.mutate({ url: '', blob: blob }); // 이미지 업로드하는 api호출, 타입 맞추기 위해 url: ''을 보냄
-    }
-  };
 
   const handleSignupClick = async () => {
     if (agreement) {
-      handleUploadImage(); // 동의하고 난 후에는 회원가입 절차니 이미지 S3 업로드
       signupEmail.mutate(signupInfo);
     }
   };
