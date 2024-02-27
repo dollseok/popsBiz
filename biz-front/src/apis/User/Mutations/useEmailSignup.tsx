7cc858f2f1072d7d76ee6b12a4879aee2bd7dfb9
이은석 <modelism93@naver.com>
2024-02-27 12:00:04 +0900

    [FE] rename: useEmailSignup으로 파일 이름, 함수 이름 변경

--- biz-front/src/apis/User/Mutations/useEmailSignup.tsx
+++ biz-front/src/apis/User/Mutations/useEmailSignup.tsx
@@ -1,32 +1,37 @@
 import { apiErrorType, apiSuccessType } from '@/types/apiResponse';
-import { SignupEmailInfo } from '@/types/user';
+import { SignupUserType } from '@/types/user';
 import { useMutation } from '@tanstack/react-query';
 import { SignupEmail } from '../userAPI';
 import { useNavigate } from 'react-router';
 import { PATH } from '@/constants/path';
+import { signupInfoState } from '@/states/User';
+import { useRecoilValue } from 'recoil';
 
-const useSignupEmail = () => {
+const useEmailSignup = () => {
   const navigate = useNavigate();
+  const signupInfo = useRecoilValue(signupInfoState); // 회원가입에 요구되는 데이터
 
-  return useMutation<apiSuccessType, apiErrorType, SignupEmailInfo>({
+  return useMutation<apiSuccessType, apiErrorType, SignupUserType>({
     mutationFn: data => {
+      console.log(signupInfo);
+      console.log('이메일로 회원가입 데이터', data);
       return SignupEmail(data);
     },
     onSuccess: data => {
       console.log(data);
       // 에러에 따른 리턴
       if (data.result === 'error') {
-        console.log('에러');
+        console.log('회원 가입 실패');
       }
       // 성공에 따른 리턴
       else {
-        console.log('성공');
+        console.log('회원 가입 성공');
         // TODO: 자동 로그인? 할거인지
-        navigate(PATH.ROOT);
+        // navigate(PATH.ROOT);
       }
     },
     onError: () => {},
   });
 };
 
-export { useSignupEmail };
+export { useEmailSignup };
