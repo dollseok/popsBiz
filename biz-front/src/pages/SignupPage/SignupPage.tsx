a5f37f00b3567d99064c601e1d31ff397abb0c0d
이은석 <modelism93@naver.com>
2024-02-28 14:51:55 +0900

    [FE] feat: 회원 가입 시 부족한 데이터 입력 요청 에러메세지 & scrolltop

--- biz-front/src/pages/SignupPage/SignupPage.tsx
+++ biz-front/src/pages/SignupPage/SignupPage.tsx
@@ -1,36 +1,95 @@
 import { useEmailSignup } from '@/apis/User/Mutations/useEmailSignup';
-import { useUploadProfileImage } from '@/apis/User/Mutations/useUploadProfileImage';
 import { Box } from '@/components/atoms/Box/Box.styles';
 import Button from '@/components/atoms/Button/Button';
-import { Image } from '@/components/atoms/Image/Image';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
 import { GoBackNavComp } from '@/components/molecules/SignupPage/GoBackNavComp';
 import { SignupComp } from '@/components/organisms/SignupPage/SignupComp';
 import { UserdataComp } from '@/components/organisms/SignupPage/UserdataComp';
-import { agreementState, signupInfoState } from '@/states/User';
-import { useRecoilValue } from 'recoil';
+import {
+  agreeErrorState,
+  agreementState,
+  emailErrorState,
+  imageErrorState,
+  nicknameErrorState,
+  passwordErrorState,
+  signupInfoState,
+} from '@/states/User';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 const SignupPage = () => {
   const signupInfo = useRecoilValue(signupInfoState); // 회원가입에 요구되는 데이터
   const agreement = useRecoilValue(agreementState); // 필수 동의 데이터
 
+  const [emailError, setEmailError] = useRecoilState(emailErrorState);
+  const [passwordError, setPasswordError] = useRecoilState(passwordErrorState);
+  const [imageError, setImageError] = useRecoilState(imageErrorState);
+  const [nicknameError, setNicknameError] = useRecoilState(nicknameErrorState);
+  const [agreeError, setAgreeError] = useRecoilState(agreeErrorState);
+
   const EmailSignup = useEmailSignup();
+
+  // 페이지 렌더링 되었을 때 errorstate 전부 true로 -> 그래야 아무것도 아닐 때 회원가입 못하게 막음
+  useEffect(() => {
+    setEmailError(prev => ({ ...prev, state: true }));
+    setPasswordError(prev => ({ ...prev, state: true }));
+    setImageError(prev => ({ ...prev, state: true }));
+    setNicknameError(prev => ({ ...prev, state: true }));
+    setAgreeError(prev => ({ ...prev, state: true }));
+  }, []);
 
   const handleSignupClick = async () => {
     if (agreement) {
       if (
-        signupInfo.email !== '' &&
-        signupInfo.nickname !== '' &&
-        signupInfo.password !== '' &&
-        signupInfo.profileKey !== ''
+        !emailError.state &&
+        !passwordError &&
+        !imageError &&
+        !nicknameError &&
+        !agreeError
       ) {
         // 이메일 회원가입
         EmailSignup.mutate(signupInfo);
       }
-      //TODO: 데이터 부족한거 에러표시 넣어야함
+      //데이터 부족한거 에러표시 넣어야함
       else {
+        // 이메일에 문제
+        if (emailError.state) {
+          setEmailError(prev => ({
+            ...prev,
+            message: '이메일을 인증해야 회원가입이 가능합니다.',
+          }));
+        }
+        // 비밀번호에 문제
+        if (passwordError.state) {
+          setPasswordError(prev => ({
+            ...prev,
+            message: '비밀번호를 정상적으로 입력해야 회원가입이 가능합니다.',
+          }));
+        }
+        // 이미지에 문제
+        if (imageError.state) {
+          setImageError(prev => ({
+            ...prev,
+            message: '이미지를 정상적으로 등록해야 회원가입이 가능합니다.',
+          }));
+        }
+        // 닉네임에 문제
+        if (nicknameError.state) {
+          setNicknameError(prev => ({
+            ...prev,
+            message: '닉네임을 정상적으로 입력해야 회원가입이 가능합니다.',
+          }));
+        }
+        window.scrollTo(0, 0);
         console.log(signupInfo);
       }
+    }
+    // 필수 동의 안했을 때
+    else {
+      setAgreeError({
+        state: true,
+        message: '필수 약관에 동의하셔야 회원으로 가입할 수 있습니다.',
+      });
     }
   };
 
