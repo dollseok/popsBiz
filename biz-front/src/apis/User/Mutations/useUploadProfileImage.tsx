63926250c64c07ecd74b3e378220a5962672b564
이은석 <modelism93@naver.com>
2024-02-28 12:48:52 +0900

    [FE] feat: 이미지 등록 예외 처리, 에러 메세지 추가

--- biz-front/src/apis/User/Mutations/useUploadProfileImage.tsx
+++ biz-front/src/apis/User/Mutations/useUploadProfileImage.tsx
@@ -5,8 +5,9 @@ import { uploadImageType } from '@/types/user';
 import { extractIdFromUrl } from '@/utils/extractIdFromUrl';
 import { useGetPresignedUrl } from '../Queries/useGetPresignedUrl';
 import { useState } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import {
+  imageErrorState,
   signupInfoState,
   signupModeState,
   socialSignupInfoState,
@@ -21,6 +22,8 @@ const useUploadProfileImage = () => {
   const setSignupInfo = useSetRecoilState(signupInfoState); // 회원가입에 요구되는 데이터
   const setSocialSignupInfo = useSetRecoilState(socialSignupInfoState); // 소셜 로그인일때 요구되는 데이터
   const signupMode = useRecoilValue(signupModeState);
+  const setImageError = useSetRecoilState(imageErrorState);
+  const resetImageError = useResetRecoilState(imageErrorState);
 
   // response 200 이 리턴값
   return useMutation<number | undefined, apiErrorType, uploadImageType>({
@@ -36,14 +39,21 @@ const useUploadProfileImage = () => {
         // 기본 회원가입
         if (signupMode === 'basic') {
           setSignupInfo(prev => ({ ...prev, profileKey: profileKey }));
+          resetImageError();
         }
         // 소셜 회원가입
         else if (signupMode === 'social') {
           setSocialSignupInfo(prev => ({ ...prev, profileKey: profileKey }));
+          resetImageError();
         }
       }
     },
-    onError: () => {},
+    onError: () => {
+      setImageError({
+        state: true,
+        message: '파일 업로드 중 오류가 발생했습니다. 다시 시도해주세요.',
+      });
+    },
   });
 };
 
