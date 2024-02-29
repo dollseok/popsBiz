9fd7b43b557cbbeb99d669b39da16ef719ab57e3
이은석 <modelism93@naver.com>
2024-02-29 14:39:26 +0900

    [FE] feat: 로고 추가 & 홈 버튼 기능

--- biz-front/src/components/molecules/MainPage/Common/Navbar.tsx
+++ biz-front/src/components/molecules/MainPage/Common/Navbar.tsx
@@ -1,11 +1,32 @@
+import { Image } from '@/components/atoms/Image/Image';
 import { Text } from '@/components/atoms/Text/Text';
 import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
 
+import logoImage from '@/assets/images/logo.png';
+import { useNavigate } from 'react-router';
+import Button from '@/components/atoms/Button/Button';
+import { PATH } from '@/constants/path';
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleRouter = (url: string): void => {
+    navigate(url);
+  };
+
   return (
     <Wrapper $size="Nav" option="RowSideEnd">
-      <Wrapper>
-        <Text>로고</Text>
+      <Wrapper option="Row">
+        <Button
+          $width="220px"
+          option="none"
+          size="none"
+          $marginRight="20px"
+          onClick={() => handleRouter(PATH.ROOT)}
+        >
+          <Image width={120} height={27.45} $unit="px" src={logoImage} />
+        </Button>
+
         <Text>비즈 웹 사이트</Text>
       </Wrapper>
       <Text>유저 프로필</Text>
