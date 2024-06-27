f84ce4e1480a0ffc7aab452e17d9dc1deea9d290
이은석 <modelism93@naver.com>
2024-06-27 19:30:41 +0900

    [FE] 추가 정보에 입장 정보, 대표예약링크 추가

---
+++ biz-front/src/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/AdmissionInfoComp.tsx
@@ -0,0 +1,66 @@
+import { Text } from '@/components/atoms/Text/Text';
+import { Textarea } from '@/components/atoms/Textarea/Textarea';
+import { Wrapper } from '@/components/atoms/Wrapper/Wrapper';
+import { ChangeEvent, useState } from 'react';
+
+const AdmissionInfoComp = () => {
+  const [freeChecked, setFreeChecked] = useState<boolean>(false);
+  const [payChecked, setPayChecked] = useState<boolean>(false);
+  const [admissionTextInfo, setAdmissionTextInfo] = useState<string>('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.id);
+    if (e.target.id === 'free') {
+      if (!freeChecked) {
+        setPayChecked(false);
+      }
+      setFreeChecked(!freeChecked);
+    } else if (e.target.id === 'pay') {
+      if (!payChecked) {
+        setFreeChecked(false);
+      }
+      setPayChecked(!payChecked);
+    }
+  };
+
+  return (
+    <>
+      <Wrapper $marginBottom="18px">
+        <Text size="body1" $fontWeight="bold" $marginBottom="5px">
+          입장 정보
+        </Text>
+        <Text size="body4" $color="grey1" $marginBottom="10px">
+          입장 비용, 예약 정보 등 입장 정보를 통해 소비자들의 이용 편의성을
+          높여보세요.
+        </Text>
+        <Wrapper $marginBottom="8px" option="Row">
+          <input
+            type="checkbox"
+            id="free"
+            name="free"
+            checked={freeChecked}
+            onChange={e => handleChange(e)}
+          ></input>
+          <label htmlFor="free">무료</label>
+          <input
+            type="checkbox"
+            id="pay"
+            name="pay"
+            checked={payChecked}
+            onChange={e => handleChange(e)}
+          ></input>
+          <label htmlFor="free">유료</label>
+        </Wrapper>
+        <Wrapper>
+          <Textarea
+            $width="338px"
+            $height="86px"
+            onChange={e => setAdmissionTextInfo(e.target.value)}
+          ></Textarea>
+        </Wrapper>
+      </Wrapper>
+    </>
+  );
+};
+
+export default AdmissionInfoComp;
--- biz-front/src/components/organisms/MainPage/Popup/Regist/AdditionalDataComp.tsx
+++ biz-front/src/components/organisms/MainPage/Popup/Regist/AdditionalDataComp.tsx
@@ -1,11 +1,17 @@
+import AdmissionInfoComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/AdmissionInfoComp';
 import EventInputComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/EventInputComp';
 import HashtagInputComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/HashtagInputComp';
 import PopupNoticeInputComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/PopupNoticeInputComp';
+import ReserveLinkInputComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/ReserveLinkInputComp';
 import SiteInputComp from '@/components/molecules/MainPage/Popup/PopupRegist/AddtionalData/SiteInputComp';
 
 const AddtionalDataComp = () => {
   return (
     <>
+      {/* 입장 정보 */}
+      <AdmissionInfoComp />
+      {/* 대표 예약 링크 */}
+      <ReserveLinkInputComp />
       {/* 해시태그 */}
       <HashtagInputComp />
       {/* SNS/Website */}
